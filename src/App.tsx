75bbeecc07bcc01ff494b1ed509731b3676cec14
Cesar Daniel Rodrigues <cesar.daniel.rodrigues@gmail.com>
2025-03-10 15:49:26 -0300

    Tipagem de temas

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-
-
+import { Button } from "./components/Button"
+import { ThemeProvider } from "styled-components"
+import { defaultTheme } from "./styles/themes/default"
 
 function App() {
   return (
-    <h1>Hello World</h1>
-  ) 
+    <ThemeProvider theme={defaultTheme}>
+      
+      <Button variant='primary'/>
+      <Button variant='secondary'/>
+      <Button variant='danger'/>
+      <Button variant='success'/>
+    </ThemeProvider>
+  )
 }
-
 export default App
