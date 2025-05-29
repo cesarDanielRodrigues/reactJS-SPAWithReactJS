7de6a0df73a08403fd7eb930a56aa8522fe59d82
cesarDanielRodrigues <cesar.daniel.rodrigues@gmail.com>
2025-05-29 06:45:20 -0300

    Listagem do historico

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,69 +1,40 @@
-import { useContext } from "react";
-import { HistoryContainer, HistoryList, Status } from "./styles";
-import { CyclesContext } from "../../contexts/CycleContext";
+import { useContext } from "react"
+import { HistoryContainer, HistoryList, Status } from "./styles"
+import { CyclesContext } from "../../contexts/CycleContext"
 
-export function History(){
-    const {cycles} = useContext(CyclesContext)
-    return(
-       <HistoryContainer>   
-            <h1>Meu histórico</h1>
-            <pre>{JSON.stringify(cycles, null, 2)}</pre>
-            <HistoryList>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Tarefa</th>
-                            <th>Duração</th>
-                            <th>Início</th>
-                            <th>Status</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Tarefa</td>
-                            <td>20 minutos</td>
-                            <td>Há 2 meses</td>
-                            <td><Status statusProps="green">Concluído</Status></td>
-                        </tr>
-                        <tr>
-                            <td>Tarefa</td>
-                            <td>20 minutos</td>
-                            <td>Há 2 meses</td>
-                            <td><Status statusProps="yellow">Em andamento</Status></td>
-                        </tr>
-                        <tr>
-                            <td>Tarefa</td>
-                            <td>20 minutos</td>
-                            <td>Há 2 meses</td>
-                            <td><Status statusProps="red">Encerrado</Status></td>
-                        </tr>
-                        <tr>
-                            <td>Tarefa</td>
-                            <td>20 minutos</td>
-                            <td>Há 2 meses</td>
-                            <td><Status statusProps="green">Concluído</Status></td>
-                        </tr>
-                        <tr>
-                            <td>Tarefa</td>
-                            <td>20 minutos</td>
-                            <td>Há 2 meses</td>
-                            <td><Status statusProps="green">Concluído</Status></td>
-                        </tr>
-                        <tr>
-                            <td>Tarefa</td>
-                            <td>20 minutos</td>
-                            <td>Há 2 meses</td>
-                            <td><Status statusProps="green">Concluído</Status></td>
-                        </tr>
-                        <tr>
-                            <td>Tarefa</td>
-                            <td>20 minutos</td>
-                            <td>Há 2 meses</td>
-                            <td><Status statusProps="green">Concluído</Status></td>
-                        </tr>
-                    </tbody>
-                </table>
-            </HistoryList>
-       </HistoryContainer>
-    )
+export function History() {
+  const { cycles } = useContext(CyclesContext)
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cycles.map((cycle) => {
+              return (
+                <tr id={cycle.id}>
+                  <td>{cycle.task}</td>
+                  <td>{cycle.minutesAmount} minutos</td>
+                  <td>Há 2 meses</td>
+                  <td>
+                    {cycle.finishedDate && <Status statusProps="green">Concluído</Status>}
+                    {cycle.interruptedDate && <Status statusProps="red">Interrompido</Status>}
+                    {!cycle.finishedDate && !cycle.interruptedDate && <Status statusProps="yellow">Em andamento</Status>}
+                </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
 }
